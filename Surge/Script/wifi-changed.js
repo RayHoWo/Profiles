6fe96564a7c2e7be058b9a5ee1d03487090b6531
Ray <58543143+RayHoWo@users.noreply.github.com>
2021-07-28 14:30:21 +0800

    updates

--- Surge/Script/wifi-changed.js
+++ Surge/Script/wifi-changed.js
@@ -3,7 +3,7 @@
  * 如果是家里WI-FI则开启直连模式
  * 如果不是家里WI-FI则开启代理模式
  */
-const WIFI_DONT_NEED_PROXYS = ['CynEu-Orbi'];
+const WIFI_DONT_NEED_PROXYS = ['师盟'];
 if (wifiChanged()) {
   if (WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)) {
     $surge.setOutboundMode('direct');
